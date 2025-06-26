7e7eaa0d5e6ed3fb716a0d6068970f4c8f4efcb4
unknown <damienbacadev@gmail.com>
2025-06-25 21:24:30 -0600

    Updating game_state_manager.py - Moving globally defined variables to class variables

--- Tic-Tac-Toe Game/game_state_manager.py
+++ Tic-Tac-Toe Game/game_state_manager.py
@@ -6,29 +6,29 @@ from menu import Menu
 from single_player import SinglePlayer
 from multi_player import MultiPlayer
 
-FPS = 60
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 500
-
-INITIAL_STATE = 'menu'
-
-MAIN_MENU_STATE = 'menu'
-SINGLE_PLAYER_STATE = 'singlePlayer'
-MULTI_PLAYER_STATE = 'multiplayer'
-STATES = {
-    MAIN_MENU_STATE: Menu(),
-    SINGLE_PLAYER_STATE: SinglePlayer(),
-    MULTI_PLAYER_STATE: MultiPlayer()
-}
-
 
 class GameStateManager:
+    FPS = 60
+    SCREEN_WIDTH = 500
+    SCREEN_HEIGHT = 500
+    INITIAL_STATE = 'menu'
+    MAIN_MENU_STATE = 'menu'
+    SINGLE_PLAYER_STATE = 'singlePlayer'
+    MULTI_PLAYER_STATE = 'multiplayer'
+
+    STATES = {
+        MAIN_MENU_STATE: Menu(),
+        SINGLE_PLAYER_STATE: SinglePlayer(),
+        MULTI_PLAYER_STATE: MultiPlayer()
+    }
+
     def __init__(self):
         pygame.init()
         self.done = False
-        self.state = STATES[INITIAL_STATE]
+        self.state = self.STATES[self.INITIAL_STATE]
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode(
+            (self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
 
         pygame.display.set_caption("Tic Tac Toe")
 
@@ -50,7 +50,7 @@ class GameStateManager:
 
     def game_loop(self):
         while not self.done:
-            dt = self.clock.tick(FPS) / 1000.0  # frame time
+            dt = self.clock.tick(self.FPS) / 1000.0  # frame time
             self.event_loop()
             self.update(dt)
             pygame.display.update()
